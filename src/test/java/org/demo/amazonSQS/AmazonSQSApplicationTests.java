339934afbd8848e3a9e405e6cf8a84fa23a044cb
1209simran <1209simran@gmail.com>
2024-09-05 00:42:45 +0530

    aws Simple Queue service

---
+++ src/test/java/org/demo/amazonSQS/AmazonSQSApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.amazonSQS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonSQSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
